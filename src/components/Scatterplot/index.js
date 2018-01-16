1cea5b8a91eebcdf8a5db86ff6dd16f025289871
Sebastian Basznianin <avaven@gmail.com>
2018-01-17 00:23:00 +0100

    Removed default hover behavior, added styling, further work on scaling

--- src/components/Scatterplot/index.js
+++ src/components/Scatterplot/index.js
@@ -12,36 +12,42 @@ class Scatterplot extends Component {
       position: 'top',
       labels: {
         usePointStyle: true,
+        fontColor: '#8e8e8e',
+        fontFamily: 'sans-serif',
+        fontSize: 16,
+        padding: 24,
       },
-      onClick: () => {}
+      onClick: null
     }
 
     const data = {
       datasets: [{
         label: 'pass',
+        backgroundColor: 'rgba(101,182,60,1)',
         pointBackgroundColor: 'rgba(101,182,60,1)',
         pointRadius: 10,
-        pointHitRadius: 15,
         data: this.props.plotpoints.filter((item) => item.status === 'pass')
       },
         {
           label: 'error',
+          backgroundColor: 'rgba(245,128,49,1)',
           pointBackgroundColor: 'rgba(245,128,49,1)',
           pointRadius: 10,
           data: this.props.plotpoints.filter((item) => item.status === 'error')
-
         },
         {
           label: 'fail',
+          backgroundColor: 'rgba(232,55,63,1)',
           pointBackgroundColor: 'rgba(232,55,63,1)',
           pointRadius: 10,
           data: this.props.plotpoints.filter((item) => item.status === 'fail')
-
         }]
     }
 
     const chartConfig = {
-      
+      hover: {
+        mode: false
+      },
       tooltips: {
         enabled: false
       },
@@ -50,7 +56,10 @@ class Scatterplot extends Component {
           gridLines: {
             display: false,
           },
-
+          ticks: {
+            fontSize: 20,
+            fontColor: '#B6BEC6',
+          },
           type: 'time',
           time: {
             displayFormats: {
@@ -61,12 +70,17 @@ class Scatterplot extends Component {
         yAxes: [{
           gridLines: {
             borderDash: [8, 8],
+            lineWidth: 2,
+            zeroLineColor: 'rgba(230,234,239,1)',
+            zeroLineWidth: 2
           },
           ticks: {
+            fontSize: 20,
+            fontColor: '#B6BEC6',
             min: 0,
             max: 300,
             stepSize: 30,
-            callback: (value, index, values) => value % 60 === 0 ? `${value / 60} min` : ''
+            callback: (value, index, values) => value % 60 === 0 && value !== 0 ? `${value / 60} min` : ''
           }
         }]
       }
