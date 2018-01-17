8c2587444cf6f68293506152ae7d26fe05e055a8
Sebastian Basznianin <avaven@gmail.com>
2018-01-17 02:50:28 +0100

    Code refactoring

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+const PORT = process.env.PORT || 5000
+
+app.get('/plotpoints', (req, res) => {
+  res.json()
+})
+
+app.listen(PORT)
